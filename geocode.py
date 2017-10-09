9dc1163de90a21334b9374fe916b17ebdfa93ce2
ppbp <32603918+ppbp@users.noreply.github.com>
2017-10-09 17:00:07 -0400

    JSON Output
    Reads a CSV with at least "Address" and "City" fields and converts it to JSON format. To include lat/longs, the "-g" or "--geocode" parameter needs to be set to "on." Default is "off" since it requires a webservice call to ArcGIS; it's faster for testing purpose to have default off.

--- geocode.py
+++ geocode.py
@@ -5,27 +5,25 @@
 # create a variable input for the geocoder because it only accepts 1 argument; we want the Address, City, St to be in 1 line
 
 import geocoder, csv
+import argparse
+import datetime
+import json
 
-with open('sample.csv', 'rb') as input, open('sampleLatLon.csv', 'wb') as output:
-    reader = csv.reader(input, delimiter=',')
-    row0 = reader.next()
-    print row0
-    Longitude = ['lon']
-    Latitude = ['lat']
-    new_line = row0 + Longitude + Latitude
-    print new_line
-    output.writelines([",".join(new_line)] + ['\n'])
+parser = argparse.ArgumentParser(description='-----------Geocoding Help-----------')
+parser.add_argument("-i", "--input", help='Required input CSV file name', required=True)
+parser.add_argument("-g", "--geocode", help='\'on\' or \'off\' to get geocodes from ArcGIS. Default is \'off\' since it\'s faster.')
+args = parser.parse_args()
 
+address_file = args.input
+output_file = 'addresses_out.json'
 
+with open(address_file, 'rb') as input, open(output_file, 'wb') as output:
+    reader = csv.DictReader(input)
     for row in reader:
-        print row
-        address = ', '.join(row[1:4]) # geocoder accepts only 1 arg, combines row[1] + row[2] + row[3]
-        print address
-        g = geocoder.arcgis(address) #change arcgis to google to switch services
-        newLat = g.lat
-        newLon = g.lng
-        print newLat + newLon
-        row_line = row + [str(newLon)] + [str(newLat)]
-        print row_line
-        output.writelines(','.join(row_line) + '\n')
+        if args.geocode == 'on':
+            address = row['Address'] + ', ' + row['City'] + ', TX'
+            row['Latitude'] =  geocoder.arcgis(address).lat
+            row['Longitude'] = geocoder.arcgis(address).lng
 
+        json.dump(row, output, indent=4, sort_keys=True)
+        output.write('\n')
